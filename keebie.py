02d48a454cc06fbfb1eb8feffc176b9850ab7bdc
Michael-E-B <MichaelBasaj@protonmail.com>
2021-03-29 15:12:19 -0400

    Rearanges definitions in `keebie.py`.
    It was an absolute mess, so I made a massive mergeablity mess for any parallel commits.

--- keebie.py
+++ keebie.py
@@ -10,19 +10,36 @@ import argparse
 import time
 import subprocess
 
+
+
+# Global vars
+
 filePath = os.path.abspath(os.path.dirname(sys.argv[0])) + "/" # Get the absolute path to the directory of this script for use when opening files
 
-settings = { # A dict of settings to be used across the script
-    "multiKeyMode": "combination",
-    "forceBackground": False,
-    "backgroundInversion": False
-}
+layerDir = filePath + "layers/" # Cache the full path to the /layers directory
+scriptDir = filePath + "scripts/" # Cache the full path to the /scripts directory
 
-settingsPossible = { # A dict of lists of valid values for each setting
-    "multiKeyMode": ["combination", "sequence"],
-    "forceBackground": [True, False],
-    "backgroundInversion": [True, False]
-}
+
+
+# Signal handling
+
+def signal_handler(signal, frame):
+    end()
+
+def end(): # Properly close the device file and exit the script
+    close(device)
+    sys.exit(0)
+
+def close(self): # try to close the device file gracefully
+    if self.fd > -1:
+        try:
+            os.close(self.fd)
+        finally:
+            self.fd = -1
+
+
+
+# Key Ledger
 
 class keyLedger():
     """A class for finding all keys pressed at any time."""
@@ -139,21 +156,11 @@ class keyLedger():
             print(f"Unrecognized value for returnType: {returnType}, returning None, expect errors!") # Say so
             return None
 
-def signal_handler(signal, frame):
-    end()
 
-def end(): # Properly close the device file and exit the script
-    close(device)
-    sys.exit(0)
 
-def close(self): # try to close the device file gracefully
-    if self.fd > -1:
-        try:
-            os.close(self.fd)
-        finally:
-            self.fd = -1
+# Config file
 
-def config(): # Open the config file and return a list of it's line with leading and trailing spaces striped
+def config(): # Open the config file and return a list of it's lines with leading and trailing spaces striped
     f=open(filePath+"config","r") # Opens config file.
 
     if f.mode =='r':
@@ -169,19 +176,161 @@ def writeConfig(lineNum, data): # Writes some data to a line of the config file
     out.writelines(lines)
     out.close()
 
-layerDir = filePath + "layers/" # Cache the full path to the /layers directory
-scriptDir = filePath + "scripts/" # Cache the full path to the /scripts directory
 
-parser = argparse.ArgumentParser() # Set up command line arguments
-parser.add_argument("--layers", help="Show saved layer files", action="store_true")
-parser.add_argument("--device", help="Change target device")
-parser.add_argument("--detect", help="Detect keyboard device file", action="store_true")
-parser.add_argument("--add", help="Add new keys", action="store_true")
-parser.add_argument("--settings", help="Edits settings file", action="store_true")
-parser.add_argument("--edit", help="Edits specified layer file (or default layer if unspecified)", nargs="?", default=False, const="default.json", metavar="layer", choices=[i for i in os.listdir(layerDir) if os.path.splitext(i)[1] == ".json"])
-args = parser.parse_args()
 
-print("Welcome to Keebie")
+# JSON
+
+def readJson(filename, dir = layerDir): # Reads the file contents of a layer (or any json file named filename in the directory dir)
+    with open(dir+filename) as f:
+        data = json.load(f)
+
+    return data 
+
+def writeJson(filename, data, dir = layerDir): # Appends new data to a specified layer (or any json file named filename in the directory dir)
+    with open(dir+filename) as f:
+        prevData = json.load(f)
+
+    prevData.update(data)
+
+    with open(dir+filename, 'w+') as outfile:
+        json.dump(prevData, outfile, indent=3)
+
+def popJson(filename, key, dir = layerDir): # Removes the key key and it's value from a layer (or any json file named filename in the directory dir)
+    with open(dir+filename) as f:
+        prevData = json.load(f)
+
+    prevData.pop(key)
+
+    with open(dir+filename, 'w+') as outfile:
+        json.dump(prevData, outfile, indent=3)
+
+
+
+# Layer file
+
+def createLayer(filename): # Creates a new layer with a given filename
+    basedata = {"KEY_ESC": "layer:default", "leds": []}
+
+    with open(layerDir+filename, 'w+') as outfile:
+        json.dump(basedata, outfile, indent=3)
+
+
+
+# Settings file
+
+settings = { # A dict of settings to be used across the script
+    "multiKeyMode": "combination",
+    "forceBackground": False,
+    "backgroundInversion": False
+}
+
+settingsPossible = { # A dict of lists of valid values for each setting
+    "multiKeyMode": ["combination", "sequence"],
+    "forceBackground": [True, False],
+    "backgroundInversion": [True, False]
+}
+
+def getSettings(): # Reads the json file specified on the third line of config and sets the values of settings based on it's contents
+    print(f"Loading settings from {config()[2]}") # Notify the user we are getting settings and tell them the file we are using to do so
+
+    settingsFile = readJson(config()[2], filePath) # Get a dict of the keys and values in our settings file
+    for setting in settings.keys(): # For every setting we expect to be in our settings file
+        if settingsFile[setting] in settingsPossible[setting]: # If the value in our settings file is valid
+            # print(f"Found valid value: \"{settingsFile[setting]}\" for setting: \"{setting}\"")
+            settings[setting] = settingsFile[setting] # Write it into our settins
+
+        else :
+            print(f"Value: \"{settingsFile[setting]}\" for setting: \"{setting}\" is invalid, defaulting to {settings[setting]}") # Warn the user of invalid settings in the settings file
+            
+    # print(f"Settings are {settings}") # Tell the user the settings we ended up with
+
+
+
+# LEDs
+
+def setLeds(onLeds): # Sets the passed LEDs on (and any others off)
+    leds = device.capabilities()[17] # Get a list of LEDs the device has
+
+    for led in leds: # For all LEDs on the board
+        if led in onLeds: # If the LED is to be set on
+            device.set_led(led, 1) # Set it on
+        else:
+            device.set_led(led, 0) # Set it off
+
+
+
+# Keypress processing
+
+def processKeycode(keycode): # Given a keycode that might be in the layer json file, check if it is and execute the appropriate commands
+    if keycode in readJson(config()[1]): # If the keycode is in our layer's json file
+        value = readJson(config()[1])[keycode] # Get the instructions associated with the keycode
+
+        if value.startswith("layer:"): # If value is a layerswitch command
+            if os.path.exists(layerDir+value.split(':')[-1] + ".json") == False: #if the layer has no json file
+                createLayer(value.split(':')[-1]+".json") # Create one
+                print("Created layer file: " + value.split(':')[-1]+".json") # Notify the user
+                writeConfig(1, value.split(':')[-1] + ".json") # Switch to our new layer file
+                print("Switched to layer file: " + value.split(':')[-1] + ".json") # Notify the user
+
+            else:
+                writeConfig(1, value.split(':')[-1] + ".json") # Switch the layer's json into our config
+                print("Switched to layer file: " + value.split(':')[-1] + ".json") # Notify the user
+
+            if "leds" in readJson(config()[1]):
+                setLeds(readJson(config()[1])["leds"])
+            else:
+                print(f"Layer {readJson(config()[1])} has no leds property, writing empty")
+                writeJson(config()[1], {"leds": []})
+                setLeds([])
+
+        if value.strip().endswith("&") == False and settings["forceBackground"]: # If value is not set in run in the background and our settings say to force running in the background
+            value += " &" # Force running in the background
+            
+        if value.strip().endswith("&") == False and settings["backgroundInversion"]: # If value is not set to run in the background and our settings say to invert background mode
+            value += " &" # Force running in the background
+        
+        elif value.strip().endswith("&") and settings["backgroundInversion"]: # Else if value is set to run in the background and our settings say to invert background mode
+            value = value.rstrip(" &") # Remove all spaces and &s from the end of value, there might be a better way but this is the best I've got
+
+        if value.startswith("layer:"): # If value is a layerswitch command
+            pass
+
+        elif value.startswith("script:"): # If value is a bash file
+            print("Executing bash script: " + value.split(':')[-1])
+            os.system('bash ' + scriptDir + value.split(':')[-1])
+
+        elif value.startswith("py:"): # If value is a generic python file
+            print("Executing python script: " + value.split(':')[-1])
+            os.system('python ' + scriptDir + value.split(':')[-1])
+
+        elif value.startswith("py2:"): # If value is a python2 file
+            print("Executing python2 script: " + value.split(':')[-1])
+            os.system('python2 ' + scriptDir + value.split(':')[-1])
+
+        elif value.startswith("py3:"): # If value is a python3 file
+            print("Executing python3 script: " + value.split(':')[-1])
+            os.system('python3 ' + scriptDir + value.split(':')[-1])
+        
+        elif value.startswith("exec:"): # If value is a generic executable
+            print("Executing file: " + value.split(':')[-1])
+            os.system(scriptDir + value.split(':')[-1])
+        
+        else: # If value is a shell command
+            print(keycode+": "+value)
+            os.system(value)
+
+def keebLoop(): # Reading the keyboard
+    signal.signal(signal.SIGINT, signal_handler)
+    ledger = keyLedger() # Reset the keyLedger
+
+    for event in device.read_loop(): # Start infinitely geting events from our keyboard
+        ledger.update(event) # Update the keyLedger with those events
+
+        processKeycode(ledger.getFresh(1)) # Check if ledger.freshKeysList matches a command in our layer's json file
+
+
+
+# Shells
 
 def getLayers(): # Lists all the json files in /layers and thier contents
     print("Available Layers: \n")
@@ -280,50 +429,6 @@ def addKey(key = None, command = None, keycodeTimeout = 1): # Shell for adding n
             addKey() # Restart the shell
 
         end()
-
-def writeJson(filename, data, dir = layerDir): # Appends new data to a specified layer (or any json file named filename in the directory dir)
-    with open(dir+filename) as f:
-        prevData = json.load(f)
-
-    prevData.update(data)
-
-    with open(dir+filename, 'w+') as outfile:
-        json.dump(prevData, outfile, indent=3)
-
-def createLayer(filename): # Creates a new layer with a given filename
-    basedata = {"KEY_ESC": "layer:default"}
-
-    with open(layerDir+filename, 'w+') as outfile:
-        json.dump(basedata, outfile, indent=3)
-
-def readJson(filename, dir = layerDir): # Reads the file contents of a layer (or any json file named filename in the directory dir)
-    with open(dir+filename) as f:
-        data = json.load(f)
-
-    return data 
-
-def popJson(filename, key, dir = layerDir): # Removes the key key and it's value from a layer (or any json file named filename in the directory dir)
-    with open(dir+filename) as f:
-        prevData = json.load(f)
-
-    prevData.pop(key)
-
-    with open(dir+filename, 'w+') as outfile:
-        json.dump(prevData, outfile, indent=3)
-
-def getSettings(): # Reads the json file specified on the third line of config and sets the values of settings based on it's contents
-    print(f"Loading settings from {config()[2]}") # Notify the user we are getting settings and tell them the file we are using to do so
-
-    settingsFile = readJson(config()[2], filePath) # Get a dict of the keys and values in our settings file
-    for setting in settings.keys(): # For every setting we expect to be in our settings file
-        if settingsFile[setting] in settingsPossible[setting]: # If the value in our settings file is valid
-            # print(f"Found valid value: \"{settingsFile[setting]}\" for setting: \"{setting}\"")
-            settings[setting] = settingsFile[setting] # Write it into our settins
-
-        else :
-            print(f"Value: \"{settingsFile[setting]}\" for setting: \"{setting}\" is invalid, defaulting to {settings[setting]}") # Warn the user of invalid settings in the settings file
-            
-    # print(f"Settings are {settings}") # Tell the user the settings we ended up with
 
 def editSettings(): # Shell for editing settings
     settingsFile = readJson(config()[2], filePath) # Get a dict of the keys and values in our settings file
@@ -471,82 +576,27 @@ def editLayer(layer = "default.json"): # Shell for editing a layer file (default
     else:
         end()
 
-def processKeycode(keycode): # Given a keycode that might be in the layer json file, check if it is and execute the appropriate commands
-    if keycode in readJson(config()[1]): # If the keycode is in our layer's json file
-        value = readJson(config()[1])[keycode] # Get the instructions associated with the keycode
 
-        if value.startswith("layer:"): # If value is a layerswitch command
-            if os.path.exists(layerDir+value.split(':')[-1] + ".json") == False: #if the layer has no json file
-                createLayer(value.split(':')[-1]+".json") # Create one
-                print("Created layer file: " + value.split(':')[-1]+".json") # Notify the user
-                writeConfig(1, value.split(':')[-1] + ".json") # Switch to our new layer file
-                print("Switched to layer file: " + value.split(':')[-1] + ".json") # Notify the user
+# Arguments
 
-            else:
-                writeConfig(1, value.split(':')[-1] + ".json") # Switch the layer's json into our config
-                print("Switched to layer file: " + value.split(':')[-1] + ".json") # Notify the user
+parser = argparse.ArgumentParser() # Set up command line arguments
 
-            if "leds" in readJson(config()[1]):
-                setLeds(readJson(config()[1])["leds"])
-            else:
-                print(f"Layer {readJson(config()[1])} has no leds property, writing empty")
-                writeJson(config()[1], {"leds": []})
-                setLeds([])
+parser.add_argument("--layers", help="Show saved layer files", action="store_true")
+parser.add_argument("--device", help="Change target device")
+parser.add_argument("--detect", help="Detect keyboard device file", action="store_true")
+parser.add_argument("--add", help="Add new keys", action="store_true")
+parser.add_argument("--settings", help="Edits settings file", action="store_true")
+parser.add_argument("--edit", help="Edits specified layer file (or default layer if unspecified)", nargs="?", default=False, const="default.json", metavar="layer", choices=[i for i in os.listdir(layerDir) if os.path.splitext(i)[1] == ".json"])
 
-        if value.strip().endswith("&") == False and settings["forceBackground"]: # If value is not set in run in the background and our settings say to force running in the background
-            value += " &" # Force running in the background
-            
-        if value.strip().endswith("&") == False and settings["backgroundInversion"]: # If value is not set to run in the background and our settings say to invert background mode
-            value += " &" # Force running in the background
-        
-        elif value.strip().endswith("&") and settings["backgroundInversion"]: # Else if value is set to run in the background and our settings say to invert background mode
-            value = value.rstrip(" &") # Remove all spaces and &s from the end of value, there might be a better way but this is the best I've got
+args = parser.parse_args()
 
-        if value.startswith("layer:"): # If value is a layerswitch command
-            pass
 
-        elif value.startswith("script:"): # If value is a bash file
-            print("Executing bash script: " + value.split(':')[-1])
-            os.system('bash ' + scriptDir + value.split(':')[-1])
 
-        elif value.startswith("py:"): # If value is a generic python file
-            print("Executing python script: " + value.split(':')[-1])
-            os.system('python ' + scriptDir + value.split(':')[-1])
+# Main code
 
-        elif value.startswith("py2:"): # If value is a python2 file
-            print("Executing python2 script: " + value.split(':')[-1])
-            os.system('python2 ' + scriptDir + value.split(':')[-1])
+print("Welcome to Keebie")
 
-        elif value.startswith("py3:"): # If value is a python3 file
-            print("Executing python3 script: " + value.split(':')[-1])
-            os.system('python3 ' + scriptDir + value.split(':')[-1])
-        
-        elif value.startswith("exec:"): # If value is a generic executable
-            print("Executing file: " + value.split(':')[-1])
-            os.system(scriptDir + value.split(':')[-1])
-        
-        else: # If value is a shell command
-            print(keycode+": "+value)
-            os.system(value)
-
-def keebLoop(): # Reading the keyboard
-    signal.signal(signal.SIGINT, signal_handler)
-    ledger = keyLedger() # Reset the keyLedger
-
-    for event in device.read_loop(): # Start infinitely geting events from our keyboard
-        ledger.update(event) # Update the keyLedger with those events
-
-        processKeycode(ledger.getFresh(1)) # Check if ledger.freshKeysList matches a command in our layer's json file
-
-def setLeds(onLeds): # Sets the passed LEDs on (and any others off)
-    leds = device.capabilities()[17] # Get a list of LEDs the device has
-
-    for led in leds: # For all LEDs on the board
-        if led in onLeds: # If the LED is to be set on
-            device.set_led(led, 1) # Set it on
-        else:
-            device.set_led(led, 0) # Set it off
-
+# Set up device
 if config()[0] == "/dev/input/by-id/put-your-device-name-here" and args.device == None:
     print("You have not set your device file in " + filePath + "config")
     resp = input("Would you like to detect a device by keypress now? [Y/n] ")
@@ -565,6 +615,7 @@ else:
     device = InputDevice(config()[0]) # Get a reference to the keyboard on the first line of our config file
 
 
+# Set LEDs
 if "leds" in readJson(config()[1]):
     setLeds(readJson(config()[1])["leds"])
 else:
@@ -572,7 +623,9 @@ else:
     writeJson(config()[1], {"leds": []})
     setLeds([])
 
+
 getSettings() # Get settings from the json file in config
+
 
 if args.layers: # If the user passed --layers
     getLayers() # Show the user all layer json files and their contents
