6004a5b681c884638f0bdb72087eb504aadae6b4
Inom <inomoz@users.noreply.github.com>
2022-06-20 23:06:10 +0300

    Add usage example
    Used bash multi-line comment for this example

--- setup_tools/udevRule.sh
+++ setup_tools/udevRule.sh
@@ -1,9 +1,18 @@
-
 #!/bin/sh
 
 # This script will create udevrule which will automatically give read/write permisison and run the keebie.service as soon as you plug your keyboard
 # find the values vendor-id and product-id by using lsusb command
 # Note make sure you copy keebie.service to /etc/systemd/system before executing this script as sudo
+
+# Usage example:
+: '
+# 1. find vendor/product id
+lsusb 
+# 2. make script executable
+chmod +x ./udevRule.sh
+# 3. generate udev rule file, replace 0c45 and 5004 with your vendor/product id
+sudo ./udevRule.sh 'ATTRS{idVendor}=="04d9", ATTRS{idProduct}=="0326"' 85-keebie-systemd.rules
+'
 
 touch /etc/udev/rules.d/"$2"
 cd /etc/udev/rules.d/
